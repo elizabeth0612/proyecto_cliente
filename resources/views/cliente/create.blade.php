f83b13fc5a7fb079856199f4425091fa6fda9c10
elizabeth0612 <elizabethk0612@gmail.com>
2022-11-14 10:36:31 -0500

    Cambiando de titulo

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -11,7 +11,7 @@
 		  <div class="modal-dialog" role="document">
 		    <div class="modal-content">
 		      <div class="modal-header">
-		        <h5 class="modal-title">AGREGAR </h5>
+		        <h5 class="modal-title">AGREGAR CLIENTE</h5>
 		      </div>
 		      <div class="modal-body">
 		        <div class="form-group">
