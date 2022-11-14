84d801f42be3a5934920a0c20ce50536f50990ee
elizabeth0612 <elizabethk0612@gmail.com>
2022-11-14 10:40:12 -0500

    CAMBIO DE TITULO EDITAR

--- resources/views/cliente/editar.blade.php
+++ resources/views/cliente/editar.blade.php
@@ -12,7 +12,7 @@
 		  <div class="modal-dialog" role="document">
 		    <div class="modal-content">
 		      <div class="modal-header">
-		        <h5 class="modal-title">EDITAR </h5>
+		        <h5 class="modal-title">EDITAR DE CLIENTE </h5>
 		      </div>
 		      <div class="modal-body">
 		        <div class="form-group">
