5c2a8218eaa006b335312273d024d2080a882cd0
elizabeth0612 <elizabethk0612@gmail.com>
2022-11-16 12:10:18 -0500

    AGREGADO PARA RECORRER LOS ERRORES

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -34,7 +34,15 @@
 		      <div class="modal-footer">
 		        <input type="submit"  class="btn btn-primary" value="GUARDAR"> 
 		      </div>
-		    
+		    @if ($errors->any())
+			    <div class="alert alert-danger">
+			        <ul>
+			            @foreach ($errors->all() as $error)
+			                <li>{{ $error }}</li>
+			            @endforeach
+			        </ul>
+			    </div>
+			@endif
 		    </div>
 		  </div>
 	</div>
