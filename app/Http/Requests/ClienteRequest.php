edcd48bc17e255a58ae2b3851f9589eadae4d275
elizabeth0612 <elizabethk0612@gmail.com>
2022-11-16 12:07:06 -0500

    AGREGANDO EL REQUEST

---
+++ app/Http/Requests/ClienteRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClienteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nombre' => 'required',
+            'apellido' => 'required',
+            'telefono' => 'required',
+            'dni' => 'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'AGREGAR NOMBRE',
+            'apellido.required' => 'AGREGAR APELLIDO',
+            'telefono.required' => 'AGREGAR TELEFONO',
+            'dni.required' => 'AGREGAR DNI',
+        ];
+    }
+}
